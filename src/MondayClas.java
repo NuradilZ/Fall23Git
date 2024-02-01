e1072009eeab67f888474a62f06360390d0c54d6
NuradilZ <158139136+NuradilZ@users.noreply.github.com>
2024-02-01 10:10:43 +0600

    Update MondayClas.java

--- src/MondayClas.java
+++ src/MondayClas.java
@@ -1,6 +1,7 @@
 public class MondayClas {
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println("Hello");
+        System.out.println("Hello 1");
+        System.out.println("Hello 2");
     }
 }
